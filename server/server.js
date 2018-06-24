95baf0471d02b84aeb741e7386ef333e3caf44d2
Nabeel K <nabeel.kottol@gmail.com>
2018-06-24 16:49:00 +0530

    alert others

--- server/server.js
+++ server/server.js
@@ -14,7 +14,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: "Admin",
+    text: "Welcom to the chat app",
+    createdAt: new Date().getTime()
+  });
 
+  socket.broadcast.emit('newMessage', {
+    from: "Admin",
+    text: "New user joined",
+    createdAt: new Date().getTime()
+  })
 
   socket.on('createMessage', (message) => {
     // console.log('createMessage', message);
@@ -24,7 +34,12 @@ io.on('connection', (socket) => {
        createdAt: new Date().getTime()
      }); */
 
-    io.emit('newMessage', {
+    /* io.emit('newMessage', {
+      ...message,
+      createdAt: new Date().getTime()
+    }); */
+
+    socket.broadcast.emit('newMessage', {
       ...message,
       createdAt: new Date().getTime()
     });
