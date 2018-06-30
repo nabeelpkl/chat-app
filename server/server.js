6f3b187e0681438ec742953f6717b4d838108f92
Nabeel K <nabeel.kottol@gmail.com>
2018-06-30 13:01:12 +0530

    send messages to people only in same room

--- server/server.js
+++ server/server.js
@@ -54,19 +54,27 @@ io.on('connection', (socket) => {
       ...message,
       createdAt: new Date().getTime()
     }); */
+    var user = users.getUser(socket.id);
 
-    io.emit('newMessage', generateMessage(message.from, message.text)); // connection except the one sending
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text)); // connection except the one sending
+    }
+
     callback();
   });
 
   socket.on('createLocationMessage', (coords) => {
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
+    var user = users.getUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+    }
   });
   socket.on('disconnect', () => {
     var user = users.removeUser(socket.id);
     if (user) {
       io.to(user.room).emit('updateUserList', users.getUserList(user.room));
-      io.to(user.room).emit('newMessage',generateMessage('Admin',`${user.name} has left`));
+      io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left`));
     }
   });
 });
